6b28581b385e802e482fc7e2b096bf6566eb2555
Jarno Lahtinen <lahtinen.jarno@gmail.com>
2018-02-23 10:54:16 +0200

    New build

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,148 @@
-let gulp = require('gulp');
-let LiveServer = require('gulp-live-server');
-let browserSync = require('browser-sync');
-let browserify = require('browserify');
-let source = require('vinyl-source-stream');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var argv = require('yargs').argv;
+var gulpif = require('gulp-if');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer-core');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
 
-gulp.task('live-server', function() {
-    var server = new LiveServer('server/main.js');
-    server.start();
-})
+// gulp build --production
+var production = !!argv.production;
+// determine if we're doing a build
+// and if so, bypass the livereload
+var build = argv._.length ? argv._[0] === 'build' : false;
+var watch = argv._.length ? argv._[0] === 'watch' : true;
 
-gulp.task('bundle', function() {
-    return browserify({
-        entries: 'app/main.js',
-        debug: true,   
-    })
-    .bundle()
-    .pipe(source('app.bundle.js'))
-    .pipe(gulp.dest('./.tmp'));
-})
+var tasks = {
+    // --------------------------
+    // Delete build folder
+    // --------------------------
+    clean: function(cb) {
+        del(['build/'], cb);
+    },
+    // --------------------------
+    // Copy static assets
+    // --------------------------
+    assets: function() {
+        return gulp.src('./app/assets/**/*')
+          .pipe(gulp.dest('./.tmp'));
+    },
+    // --------------------------
+    // SASS (libsass)
+    // --------------------------
+    sass: function() {
+        return gulp.src('./app/scss/*.scss')
+        // sourcemaps + sass + error handling
+        .pipe(gulpif(!production, sourcemaps.init()))
+        .pipe(sass({
+            sourceComments: !production,
+            outputStyle: production ? 'compressed' : 'nested'
+        }))
+        .on('error', handleError('SASS'))
+        // generate .maps
+        .pipe(gulpif(!production, sourcemaps.write({
+            'includeContent': false,
+            'sourceRoot': '.'
+        })))
+        // autoprefixer
+        .pipe(gulpif(!production, sourcemaps.init({
+            'loadMaps': true
+        })))
+        .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
+        // we don't serve the source files
+        // so include scss content inside the sourcemaps
+        .pipe(sourcemaps.write({
+            'includeContent': true
+        }))
+        // write sourcemaps to a specific directory
+        // give it a file and save
+        .pipe(gulp.dest('./.tmp/css'));
+    },
+    // --------------------------
+    // Browserify (bundle)
+    // --------------------------
+    browserify: function() {
+        var bundler = browserify('app/main.js', {
+            debug: !production,
+            cache: {}
+        });
+        var rebundle = function() {
+            return bundler.bundle()
+            .on('error', handleError('Browserify'))
+            .pipe(source('app.bundle.js'))
+            .pipe(gulp.dest('./.tmp'));
+        }
+        return rebundle();
+    }
+}
 
-gulp.task('serve', ['bundle', 'live-server'], function() {
+// --------------------------
+// CUSTOM TASK METHODS
+// --------------------------
+gulp.task('clean', tasks.clean);
+// for production we require the clean method on every individual task
+var req = build ? ['clean'] : [];
+// individual tasks
+gulp.task('assets', req, tasks.assets);
+gulp.task('sass', req, tasks.sass);
+gulp.task('browserify', tasks.browserify);
+
+gulp.task('browser-sync', function() {
     browserSync.init(null, {
         proxy: "http://localhost:7777",
-        port: 9000
-    })    
-})
+        port: process.env.PORT || 9000
+    });
+});
+
+
+gulp.task('reload-sass', ['sass'], function(){
+    browserSync.reload();
+});
+
+// --------------------------
+// DEV/WATCH TASK
+// --------------------------
+gulp.task('serve', ['sass', 'browserify', 'browser-sync'], function() {
+    
+    // --------------------------
+    // watch:sass
+    // --------------------------
+    gulp.watch('./app/scss/**/*.scss', ['reload-sass']);
+
+    gutil.log(gutil.colors.bgGreen('Watching for changes...'));
+});
+
+
+
+
+// ----------------------------
+// Error notification methods
+// ----------------------------
+var beep = function() {
+    var os = require('os');
+    var file = 'gulp/error.wav';
+    if (os.platform() === 'linux') {
+      // linux
+      exec("aplay " + file);
+    } else {
+      // mac
+      console.log("afplay " + file);
+      exec("afplay " + file);
+    }
+  };
+  var handleError = function(task) {
+    return function(err) {
+      beep();
+      
+        notify.onError({
+          message: task + ' failed, check the logs..',
+          sound: false
+        })(err);
+      
+      gutil.log(gutil.colors.bgRed(task + ' error:'), gutil.colors.red(err));
+    };
+  };
--- server/main.js
+++ server/main.js
@@ -2,9 +2,6 @@ let express = require('express');
 let app = new express();
 let parser = require('body-parser');
 
-//let GroceryItem = require('./models/GroceryItem.js')
-
-//require('./database.js');
 
 app.get('/', function(req, res) {
     res.render('./../app/index.ejs');
@@ -15,5 +12,3 @@ app.listen(7777);
 app.use(parser.json());
 
 app.use(parser.urlencoded( { extended: false }));
-
-//require('./routes/items.js')(app);
